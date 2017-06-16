46ee464ee18ecc6a7e5a27ea3209d6e43697ee1d
Alexander Davis <akonidavis@gmail.com>
2017-06-15 15:11:28 -1000

    another attempt at fixing the technical skills4

--- js/app.js
+++ js/app.js
@@ -83,16 +83,16 @@ d3.select("body")
           .style("background-color", "white");
       });
 
-// d3.select("body")
-//   .selectAll("tr")
-//     .on("mouseover", function(d) {
-//       d3.select(this)
-//         .style("background-color", "gold");
-//       })
-//       .on("mouseout", function(d){
-//         d3.selectAll("text.note").remove();
-//         d3.selectAll("tr")
-//           .style("background-color", "white");
-//       });
+d3.select("body")
+  .selectAll("tr")
+    .on("mouseover", function(d) {
+      d3.select(this)
+        .style("background-color", "gold");
+      })
+      .on("mouseout", function(d){
+        d3.selectAll("text.note").remove();
+        d3.selectAll("tr")
+          .style("background-color", "white");
+      });
 
 
